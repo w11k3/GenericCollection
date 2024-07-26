8432ad99582f611090e339357df8c6834a4466ea
PauloKinjo <paulo@perfectstorm.gg>
2024-07-26 10:12:04 -0400

    Implement basic LINQ example with string list. Converts to uppercase, filters and sorts items starting with 'R'. Updates list and displays results.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿public class LINQWithListCollection
+{
+    public static void Main(string[]args)
+    {
+        // populate a List of strings
+        List<string> items = new List<string>();
+        items.Add("aQua");
+        items.Add("RusT");
+        items.Add("yElLow");
+        items.Add("rEd");
+
+        // convert all strings to uppercase; select those starting with "R"
+        var startsWithR =
+            from item in items
+            let uppercaseString = item.ToUpper()
+            where uppercaseString.StartsWith("R")
+            orderby uppercaseString
+            select uppercaseString;
+
+        // display query results
+        foreach (var item in startsWithR)
+            Console.Write($"{item} ");
+
+        Console.WriteLine();
+
+        items.Add("rUbY");
+        items.Add("SaFfRon");
+
+        //display updated query results
+        foreach(var item in startsWithR)
+            Console.Write($"{item} ");
+
+        Console.WriteLine();
+    } // end Main
+} // end class
